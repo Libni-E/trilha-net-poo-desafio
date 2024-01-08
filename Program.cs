8f8421f110be6f491e0dc0f3a0fc7027796fe20c
Libni.Dev <libnievanio@gmail.com>
2024-01-08 20:09:38 -0300

    Entregando projeto DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Iphone");
+Iphone iphone = new Iphone ("(11)95857-4576", "Iphone 15 plus", "1256255625", 126 );
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n\n\n");
+
+Console.WriteLine("Nokia");
+Nokia nokia = new Nokia ("(11)97458-9546", "Nokia 5s", "1555415455", 126 );
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
